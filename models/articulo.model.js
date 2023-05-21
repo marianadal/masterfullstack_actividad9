af99ca1f0b8b8c546a011ac90763fa0f9688fb2b
Maria Nadal <nadal.miro@gmail.com>
2023-05-21 15:35:44 +0200

    End-Point GET: Artículos por autor

--- models/articulo.model.js
+++ models/articulo.model.js
@@ -4,7 +4,18 @@ const getAll = () => {
   ORDER BY fecha_creacion DESC`);
 };
 
+const getByAutorId = (autorId) => {
+  return db.query(
+    `SELECT ar.id, titulo, descripcion, fecha_creacion, categoria, nombre, email, imagen FROM Actividad9_blog.articulos as ar
+INNER JOIN Actividad9_blog.autores as au ON ar.autores_id = au.id
+WHERE ar.autores_id = ?
+ORDER BY fecha_creacion DESC`,
+    [autorId]
+  );
+};
+
 // Se exportan los métodos
 module.exports = {
   getAll,
+  getByAutorId,
 };
